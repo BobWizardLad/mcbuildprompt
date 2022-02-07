60e7f61f6cecb7438dd0cc0735ae8136daa9a4fe
BobWizardLad <bobwizardlad@Gmail.com>
2022-02-07 15:39:25 -0600

    Page layout
    All planned pages are present and reachable from the index page.

--- index.py
+++ index.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def welcome_page():
-    return render_template("prompt.html")
+def build_prompt():
+    return render_template("builds.html")
 
-@app.route("/steve")
-def steve_page():
-    return render_template("prompt.html", name="Steve")
+@app.route("/decor")
+def decor_prompt():
+    return render_template("decor.html")
+
+@app.route("/lore")
+def lore_prompt():
+    return render_template("lore.html")
+
+@app.route("/rooms")
+def room_prompt():
+    return render_template("rooms.html")
+
+@app.route("/theme")
+def theme_prompt():
+    return render_template("theme.html")
