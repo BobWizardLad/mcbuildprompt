6588cbd2a9ee3dfbd1a87bdcacd14acbddaa2cef
BobWizardLad <bobwizardlad@Gmail.com>
2022-03-04 11:38:56 -0600

    Changes

---
+++ static/promptScripts.js
@@ -0,0 +1,27 @@
+// Return a string that gives a building
+function generateBuilds(txtFile) {
+    // Pick keywords
+    with f as OptionalEffectTiming()
+    // Decide adjectives/ addons
+    // Assemble phrase
+    // Return phrase
+}
+
+// Return a string that describes a decorative structure
+function generateDecor() {
+
+}
+
+// Return a string that describes a setting or event
+function generateLore() {
+
+}
+
+// Return a string that describes 
+function generateRooms() {
+
+}
+
+function generateTheme() {
+
+}
